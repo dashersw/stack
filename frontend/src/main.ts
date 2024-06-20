f86572c663523100351fb29e99d31d1bfb788e9a
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-06-20 16:26:51 +0300

    Fix typo

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+import App from './app.vue'
 import router from './router'
 
 const app = createApp(App)
