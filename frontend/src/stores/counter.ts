ffef5f27cb78a178ff2d6f2ed89904960b84e5f8
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-06-21 10:50:10 +0300

    Update object notation in pinia store

--- frontend/src/stores/counter.ts
+++ frontend/src/stores/counter.ts
@@ -1,12 +1,16 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useCounterStore = defineStore({
+  id: 'counter',
+  state: () => ({
+    count: 0
+  }),
+  getters: {
+    doubleCount: (state) => state.count * 2
+  },
+  actions: {
+    increment() {
+      this.count++
+    }
   }
-
-  return { count, doubleCount, increment }
 })
--- frontend/src/stores/user.ts
+++ frontend/src/stores/user.ts
@@ -1,23 +1,22 @@
-import { reactive } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useUserStore = defineStore('counter', () => {
-  const users = reactive([])
-
-  const fetchUsers = async () => {
-    try {
-      const response = await fetch('/api/users')
-      if (!response.ok) {
-        throw new Error('Failed to fetch users')
+export const useUserStore = defineStore({
+  id: 'user',
+  state: () => ({
+    users: []
+  }),
+  actions: {
+    async fetchUsers() {
+      try {
+        const response = await fetch('/api/users')
+        if (!response.ok) {
+          throw new Error('Failed to fetch users')
+        }
+        const data = await response.json()
+        this.users = data
+      } catch (error) {
+        console.error('Error fetching users:', error)
       }
-      const data = await response.json()
-      Object.assign(users, data)
-    } catch (error) {
-      console.error('Error fetching users:', error)
     }
   }
-
-  fetchUsers()
-
-  return { users }
 })
